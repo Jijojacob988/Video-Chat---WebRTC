46fb9aadaa25bcce860517c5a6fd84502ccdf9fb
Jijo Jacob <Jijojacob988@gmail.com>
2025-03-09 10:13:58 +0530

    handle incoming HTTP request

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ import express from "express";
 
 const app = express();
 
+// handle incoming http request
+
+app.get("/", (req, res) => {
+    console.log("GET Request /");
+    res.send("Hello From Server");
+    
+})
+
 app.listen(9000, () => {
     console.log('Server listening on port 9000');
     
