83baf019c84777557599aa3e7697398965e1e653
Jijo Jacob <Jijojacob988@gmail.com>
2025-03-09 10:27:04 +0530

    add Socket.io support and handle client connections

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+import { log } from "console";
 import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
 // handle incoming http request
 
@@ -8,9 +14,16 @@ app.get("/", (req, res) => {
     console.log("GET Request /");
     res.send("Hello From Server");
     
+});
+
+// handle socket connections
+
+io.on("connection", (socket) => {
+    console.log(`Someone connected to socket server and socket id is ${socket.id}`);
+    
 })
 
 app.listen(9000, () => {
-    console.log('Server listening on port 9000');
+    console.log(`Server listening on port 9000`);
     
 });
