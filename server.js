bbb210ba77eec69d7b5efc8f4205debb5acfb3c4
Jijo Jacob <Jijojacob988@gmail.com>
2025-03-09 10:39:30 +0530

    serve static HTML file on root route

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
-import { log } from "console";
+
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
+// file://your/system/path/file.html
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 // handle incoming http request
 
 app.get("/", (req, res) => {
     console.log("GET Request /");
-    res.send("Hello From Server");
+    res.sendFile(join(__dirname + "/app/index.html"));
     
 });
 
